8373edcc4ed956ccd43748e44986b607bf5f3dc4
immadyounas <immad.uddin84@gmail.com>
2021-03-14 12:58:39 +0100

    c

---
+++ README 2.md
@@ -0,0 +1,2 @@
+# portfolio
+ Immad Ud Din
---
+++ js/portfolio.js
@@ -0,0 +1,34 @@
+const menu = document.querySelector('.nav');
+const icon = document.querySelector('.icon');
+const close = document.querySelector('.closebtn');
+const search = document.getElementsByClassName('a.search');
+const caption = document.querySelectorAll('.portfolio a');
+
+icon.addEventListener('click', () => {
+  menu.style.width = "100%";
+})
+
+// close.addEventListener('click', () => {
+//   menu.style.display = "none";
+// })
+
+menu.addEventListener('click', (e) => {
+  let target = e.target.tagName;
+  if(target === "A") {
+    menu.style.width = "0%";
+  }
+})
+
+for(let i = 0 ; i < search.length ; i++){
+  search[i].addEventListener('click', function() {
+    for ( let i = 0 ; i < caption.length ; i++){
+        let str = caption[i].dataset.caption;
+        if(str.includes('')){
+            caption[i].style.display = "";
+        }
+        else{
+            caption[i].style.display = "none";
+        }
+    }
+  });
+}
